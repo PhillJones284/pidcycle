74254481d5d0ac1cbb3c1351c9e79ad284fc565a
PhillJones284 <52325859+PhillJones284@users.noreply.github.com>
2021-06-26 19:51:53 +0000

    number alignment fixed

--- script.js
+++ script.js
@@ -24,16 +24,13 @@ const showBox = (index) => {
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', vh+'px');
 
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', vh+'px');
-});
-
 let vw = window.innerWidth * 0.01;
 document.documentElement.style.setProperty('--vw', vw+'px');
 
 window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
     let vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', vh+'px');
     document.documentElement.style.setProperty('--vw', vw+'px');
 });
 
